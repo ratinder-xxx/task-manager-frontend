742c51aca92fc98458c4833206d0149d223bb704
Ratinder Singh <ratinderrajput999@gmail.com>
2026-04-09 10:58:08 +0530

    deployment fixes

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      input: 'index.html'
-    }
-  },
-  server: {
-    port: 3001,
-    proxy:{
-      "/api" :{
-        target: "http://localhost:3000",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  // load env variables based on mode (development/production)
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), tailwindcss()],
+    build: {
+      outDir: 'dist',
+      sourcemap: true,
+      rollupOptions: {
+        input: 'index.html'
+      }
+    },
+    server: {
+      port: 3001,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        }
       }
     }
-  },
+  }
 })
